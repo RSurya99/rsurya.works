d81288dbe2cff2935a1eb54240e110ae20a581d9
RSurya99 <rafli.rfsp@gmail.com>
2023-05-14 17:40:26 +0700

    feat: add nuxt, tailwind, and vue logo

--- src/components/logo/Base.tsx
+++ src/components/logo/Base.tsx
@@ -4,9 +4,19 @@ import HTMLLogo from './HtmlLogo'
 import FigmaLogo from './FigmaLogo'
 import TailwindCSSLogo from './TailwindCSSLogo'
 import GolangLogo from './GolangLogo'
+import VueLogo from './VueLogo'
+import NuxtLogo from './NuxtLogo'
 
 const BaseLogo = ({ componentName, ...attributes }: any) => {
-  const componentsMap: any = { React: ReactLogo, HTML: HTMLLogo, Figma: FigmaLogo, TailwindCSS: TailwindCSSLogo, Golang: GolangLogo }
+  const componentsMap: any = { 
+    React: ReactLogo, 
+    HTML: HTMLLogo, 
+    Figma: FigmaLogo, 
+    TailwindCSS: TailwindCSSLogo, 
+    Golang: GolangLogo,
+    VueJS: VueLogo,
+    NuxtJS: NuxtLogo
+  }
   const ComponentName: any = componentsMap[componentName] || ReactLogo
   return (
     <ComponentName {...attributes} />
