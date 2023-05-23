7af7819773cb521534cdbec2188d6dfb29ec014c
RSurya99 <rafli.rfsp@gmail.com>
2023-05-23 18:39:57 +0700

    feat: add JS, TS, Laravel, PHP, Next, and SASS logo

---
+++ src/components/logo/AlpineLogo.tsx
@@ -0,0 +1,7 @@
+import { SVGProps } from 'react'
+
+export default function SkillIconsAlpinejsDark(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 256 256" {...props}><g fill="none"><rect width="256" height="256" fill="#242938" rx="60"></rect><path fill="#D8DEE9" fillRule="evenodd" d="M180.778 84L223 126.037l-42.222 42.037l-42.223-42.037L180.778 84Z" clipRule="evenodd"></path><path fill="#77C1D2" fillRule="evenodd" d="m75.222 84l87.532 87.148H78.31L33 126.037L75.222 84Z" clipRule="evenodd"></path></g></svg>
+  )
+}
--- src/components/logo/Base.tsx
+++ src/components/logo/Base.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import ReactLogo from './ReactLogo'
-import HTMLLogo from './HtmlLogo'
+import HTMLLogo from './HTMLLogo'
 import FigmaLogo from './FigmaLogo'
 import TailwindCSSLogo from './TailwindCSSLogo'
 import GolangLogo from './GolangLogo'
 import VueLogo from './VueLogo'
 import NuxtLogo from './NuxtLogo'
+import JavascriptLogo from './JavascriptLogo'
+import TypescriptLogo from './TypescriptLogo'
+import SASSLogo from './SASSLogo'
+import AlpineLogo from './AlpineLogo'
+import PHPLogo from './PHPLogo'
+import LaravelLogo from './LaravelLogo'
+import NextLogo from './NextLogo'
 
 const BaseLogo = ({ componentName, ...attributes }: any) => {
   const componentsMap: any = { 
@@ -15,7 +22,14 @@ const BaseLogo = ({ componentName, ...attributes }: any) => {
     TailwindCSS: TailwindCSSLogo, 
     Golang: GolangLogo,
     VueJS: VueLogo,
-    NuxtJS: NuxtLogo
+    NuxtJS: NuxtLogo,
+    NextJS: NextLogo,
+    Javascript: JavascriptLogo,
+    Typescript: TypescriptLogo,
+    SASS: SASSLogo,
+    Alpine: AlpineLogo,
+    PHP: PHPLogo,
+    Laravel: LaravelLogo
   }
   const ComponentName: any = componentsMap[componentName] || ReactLogo
   return (
