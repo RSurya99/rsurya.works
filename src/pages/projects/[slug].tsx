432230f0992437ffb0d3f001c582583b5e362efa
RSurya99 <rafli.rfsp@gmail.com>
2023-05-23 20:33:18 +0700

    fix: error in some projects content and fix no cover in project detail

--- src/pages/projects/[slug].tsx
+++ src/pages/projects/[slug].tsx
@@ -13,29 +13,29 @@ import { format } from 'date-fns'
 const ProjectDetail = ({ meta, source }: any) => {
   return (
     <section className='w-full max-w-5xl mx-auto px-4 py-12 space-y-8'>
-      <Image src={meta.cover} width={1024} height={728} alt={meta.title + ' Post'}  className='rounded-xl' />
-        <div className="flex flex-wrap items-center text-base sm:text-lg gap-4 md:gap-8 font-medium mb-4">
-          <a href="#" className='underline'>#{meta.category}</a>
-          <div className="flex items-center">
-            <IconCalendar className='mr-1.5' />
-            {format(new Date(meta.date), 'MMM dd, yyyy')}
-          </div>
-          <div className="flex items-center">
-            <IconClockHour3 className='mr-1.5' />
-            {meta.readTime}
-          </div>
-          {meta.repository && <a href={meta.repository} target='_blank' className="flex items-center underline">
-            <IconBrandGithub className='mr-1.5' />
-            Repository
-          </a>}
-          {meta.link && <a href={meta.link} target='_blank' className="flex items-center underline">
-            <IconExternalLink className='mr-1.5' />
-            Live site
-          </a>}
+      {meta.cover && <Image src={meta.cover} width={1024} height={728} alt={meta.title + ' Post'}  className='rounded-xl' />}
+      <div className="flex flex-wrap items-center text-base sm:text-lg gap-4 md:gap-8 font-medium mb-4">
+        <a href="#" className='underline'>#{meta.category}</a>
+        <div className="flex items-center">
+          <IconCalendar className='mr-1.5' />
+          {format(new Date(meta.date), 'MMM dd, yyyy')}
         </div>
-        <div className="prose sm:prose-lg dark:prose-invert">
-          <MDXRemote {...source} components={MDXComponents} />
+        <div className="flex items-center">
+          <IconClockHour3 className='mr-1.5' />
+          {meta.readTime}
         </div>
+        {meta.repository && <a href={meta.repository} target='_blank' className="flex items-center underline">
+          <IconBrandGithub className='mr-1.5' />
+          Repository
+        </a>}
+        {meta.link && <a href={meta.link} target='_blank' className="flex items-center underline">
+          <IconExternalLink className='mr-1.5' />
+          Live site
+        </a>}
+      </div>
+      <div className="prose sm:prose-lg dark:prose-invert">
+        <MDXRemote {...source} components={MDXComponents} />
+      </div>
     </section>
   )
 }
